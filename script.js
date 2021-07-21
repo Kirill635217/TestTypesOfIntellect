e3038c4cadf2892383a5fdc939a54c2540cda4db
janedeli81 <jnedeli81@gmail.com>
2021-07-21 13:09:28 +0300

    Commented out unwanted code. Cards now dont flip and text (point and timer) has been removed

--- script.js
+++ script.js
@@ -6,16 +6,16 @@ BRAINYMO.Game = (function() {
     var numOfCards;
     var cardHitCounter = 0;
     var card;
-    var timer;
+    // var timer;
     var storage;
-    var points = 10;
+    // var points = 10;
 
     /**
      * Method that will be invoked on card click
      */
     function handleCardClick() {
-        if(points <= 0)
-            return;
+        // if(points <= 0)
+        //     return;
         var connection = $(this).data('connection');
         var hit;
 
@@ -25,65 +25,64 @@ BRAINYMO.Game = (function() {
             $(this).addClass('active');
             activeCards.push($(this));
 
-            // If user click on two cards then check
-            if (activeCards.length == 2) {
-                hit = checkActiveCards(activeCards);
-            }
+            // // If user click on two cards then check
+            // if (activeCards.length == 2) {
+            //     hit = checkActiveCards(activeCards);
+            // }
 
-            if (hit === true) {
-                cardHitCounter++;
-                activeCards[0].add(activeCards[1]).unbind().addClass('wobble cursor-default');
-                activeCards = [];
+            // if (hit === true) {
+            //     cardHitCounter++;
+            //     activeCards[0].add(activeCards[1]).unbind().addClass('wobble cursor-default');
+            //     activeCards = [];
 
-                // Game End
-                if(cardHitCounter === (numOfCards / 2)) {
-                    // Reset active cards
-                    activeCards = [];
-                    // Reset counter
-                    cardHitCounter = 0;
-                    // End game
-                    endGame();
-                }
-            }
-            else if(hit === false){
-                points--;
-                document.getElementById("points").textContent=points + "";
-                if(points === 0)
-                    endGame();
-            }
+            //     // Game End
+            //     if(cardHitCounter === (numOfCards / 2)) {
+            //         // Reset active cards
+            //         activeCards = [];
+            //         // Reset counter
+            //         cardHitCounter = 0;
+            //         // End game
+            //         endGame();
+            //     }
+            // }
+            // else if(hit === false){
+                // points--;
+                // document.getElementById("points").textContent=points + "";
+                // if(points === 0)
+                //     endGame();
+            // }
             // In case when user open more then 2 cards then automatically close first two
-            else if(activeCards.length === 3) {
-                for(var i = 0; i < activeCards.length - 1; i++) {
-                    activeCards[i].removeClass('active');
-                }
-                activeCards.splice(0, 2);
-            }
+            // else if(activeCards.length === 3) {
+            //     for(var i = 0; i < activeCards.length - 1; i++) {
+            //         activeCards[i].removeClass('active');
+            //     }
+            //     activeCards.splice(0, 2);
+            // }
         }
     }
 
     function endGame() {
-        timer.stopTimer();
+        // timer.stopTimer();
         // Retrieve current time
-        var time = timer.retrieveTime();
+        // var time = timer.retrieveTime();
 
         // Retrieve time from storage
-        var timeFromStorage = storage.retrieveBestTime();
-        hideShow();
+        // var timeFromStorage = storage.retrieveBestTime();
         // if there's already time saved in storage check if it's better than current one
-        if (timeFromStorage != undefined && timeFromStorage != '' && points > 0) {
-            // if current game time is better than one saved in store then save new one
-            if (time.minutes < timeFromStorage.minutes || (time.minutes == timeFromStorage.minutes && time.seconds < timeFromStorage.seconds) ) {
-                storage.setBestTime(time);
-            }
-        }
-        // else if time is not saved in storage save it
-        else if(points > 0){
-            storage.setBestTime(time);
-        }
+        // if (timeFromStorage != undefined && timeFromStorage != '' && points > 0) {
+        //     // if current game time is better than one saved in store then save new one
+        //     if (time.minutes < timeFromStorage.minutes || (time.minutes == timeFromStorage.minutes && time.seconds < timeFromStorage.seconds) ) {
+        //         storage.setBestTime(time);
+        //     }
+        // }
+        // // else if time is not saved in storage save it
+        // else if(points > 0){
+        //     storage.setBestTime(time);
+        // }
         // var restartButton = document.getElementById("btn-start");
         // restartButton.remove();
         // Update best time
-        timer.updateBestTime();
+        // timer.updateBestTime();
         // var delayInMilliseconds =3500000; 
 
         // setTimeout(function() { 
@@ -98,7 +97,7 @@ BRAINYMO.Game = (function() {
     }
 
     function checkActiveCards(connections) {
-        return connections[0].data('connection') === connections[1].data('connection');
+        // return connections[0].data('connection') === connections[1].data('connection');
     }
 
     return function(config) {
@@ -107,32 +106,30 @@ BRAINYMO.Game = (function() {
          * Main method for game initialization
          */
         this.startGame = function() {
-            points = 10;
-            document.getElementById("points").textContent=points + "";
+            // points = 10;
+            // document.getElementById("points").textContent=points + "";
             card = new BRAINYMO.Card();
-            timer = new BRAINYMO.Timer();
+            // timer = new BRAINYMO.Timer();
             storage = new BRAINYMO.Storage();
             numOfCards = config.cards.length;
             // storage.setBestTime(null);
-            card.attachCardEvent(handleCardClick, config);
+            // card.attachCardEvent(handleCardClick, config);
         };
 
         /**
          * After game initialization call this method in order to generate cards
          */
         this.generateCardSet = function() {
-            points = 10;
-            document.getElementById("points").textContent=points + "";
             // Generate new card set
             card.generateCards(config.cards);
             // Reset active cards array
             activeCards = [];
-            if(document.getElementById("btn-start").textContent==="Заново"){
-                refreshPage();}
+            // if(document.getElementById("btn-start").textContent==="Заново"){
+            //     refreshPage();}
             // Reset timer
-            timer.stopTimer();
+            // timer.stopTimer();
             // Set timer
-            timer.startTimer();
+            // timer.startTimer();
         };
 
         this.startGame();
@@ -237,74 +234,73 @@ BRAINYMO.Card = (function () {
          * @param {Function} func - function that will be invoked on card click
          */
         this.attachCardEvent = function(func) {
-            $cardsContainer.unbind().on('click', '.flip-container', function() {
-                func.call(this);
-            });
+            // $cardsContainer.unbind().on('click', '.flip-container', function() {
+            //     func.call(this);
+            };
         }
     }
+    )();
+
+// BRAINYMO.Timer = (function() {
+
+//     var $timer = $('.timer');
+//     var $seconds = $timer.find('#seconds');
+//     var $minutes = $timer.find('#minutes');
+//     var $bestTimeContainer = $timer.find('.time');
+
+
+//     var minutes, seconds;
     
-})();
+//     function decorateNumber(value) {
+//         return value > 9 ? value : '0' + value;
+//     }
 
-BRAINYMO.Timer = (function() {
-
-    var $timer = $('.timer');
-    var $seconds = $timer.find('#seconds');
-    var $minutes = $timer.find('#minutes');
-    var $bestTimeContainer = $timer.find('.time');
-
-
-    var minutes, seconds;
-    
-    function decorateNumber(value) {
-        return value > 9 ? value : '0' + value;
-    }
-
-    return function() {
-        var interval;
-        var storage = new BRAINYMO.Storage();
+//     return function() {
+//         var interval;
+//         var storage = new BRAINYMO.Storage();
         
-        this.startTimer = function() {
-            var sec = 0;
-            var bestTime;
+//         this.startTimer = function() {
+//             var sec = 0;
+//             var bestTime;
 
-            // Set timer interval
-            interval = setInterval( function() {
-                seconds = ++sec % 60;
-                minutes = parseInt(sec / 60, 10);
-                $seconds.html(decorateNumber(seconds));
-                $minutes.html(decorateNumber(minutes));
-            }, 1000);
+//             // Set timer interval
+//             interval = setInterval( function() {
+//                 seconds = ++sec % 60;
+//                 minutes = parseInt(sec / 60, 10);
+//                 $seconds.html(decorateNumber(seconds));
+//                 $minutes.html(decorateNumber(minutes));
+//             }, 1000);
 
-            // Show timer
-            $timer.delay(1000).fadeIn();
+//             // Show timer
+//             $timer.delay(1000).fadeIn();
 
-            this.updateBestTime();
-        };
+//             this.updateBestTime();
+//         };
 
-        this.updateBestTime = function() {
-            // Check if user have saved best game time
-            bestTime = storage.retrieveBestTime();
-            if(bestTime != undefined && bestTime != '') {
-                $bestTimeContainer
-                    .find('#bestTime')
-                    .text(bestTime.minutes + ':' + bestTime.seconds)
-                    .end()
-                    .fadeIn();
-            }
-        };
+//         this.updateBestTime = function() {
+//             // Check if user have saved best game time
+//             bestTime = storage.retrieveBestTime();
+//             if(bestTime != undefined && bestTime != '') {
+//                 $bestTimeContainer
+//                     .find('#bestTime')
+//                     .text(bestTime.minutes + ':' + bestTime.seconds)
+//                     .end()
+//                     .fadeIn();
+//             }
+//         };
         
-        this.stopTimer = function() {
-            clearInterval(interval);  
-        };
+//         this.stopTimer = function() {
+//             clearInterval(interval);  
+//         };
 
-        this.retrieveTime = function() {
-            return {
-                minutes: decorateNumber(minutes),
-                seconds: decorateNumber(seconds)
-            }
-        };
-    }
-})();
+//         this.retrieveTime = function() {
+//             return {
+//                 minutes: decorateNumber(minutes),
+//                 seconds: decorateNumber(seconds)
+//             }
+//         };
+//     }
+// })();
 
 
 BRAINYMO.Storage = (function() {
